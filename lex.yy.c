1d37d5a214e58e5c5b026a174fb25d5d03726361
NivekTakedown <kramosl@unal.edu.co>
2022-05-29 11:46:20 -0500

    se añadío categoría lexica entero

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 8
-#define YY_END_OF_BUFFER 9
+#define YY_NUM_RULES 9
+#define YY_END_OF_BUFFER 10
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,11 +360,12 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[29] =
+static const flex_int16_t yy_accept[35] =
     {   0,
-        0,    0,    9,    8,    1,    8,    8,    1,    0,    0,
-        2,    0,    0,    0,    7,    0,    0,    4,    0,    0,
-        0,    0,    3,    0,    0,    6,    5,    0
+        0,    0,   10,    9,    1,    9,    9,    9,    1,    0,
+        0,    2,    0,    0,    0,    0,    8,    0,    0,    5,
+        0,    0,    0,    0,    0,    4,    0,    0,    0,    7,
+        0,    6,    3,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -380,8 +381,8 @@ static const YY_CHAR yy_ec[256] =
         6,    6,    6,    6,    6,    6,    6,    6,    6,    6,
         1,    1,    1,    1,    6,    1,    6,    6,    6,    6,
 
-        6,    6,    6,    6,    6,    6,    6,    6,    6,    6,
-        6,    6,    6,    6,    6,    6,    6,    6,    6,    6,
+        8,    6,    6,    6,    6,    6,    6,    6,    6,    9,
+       10,    6,    6,   11,    6,   12,    6,    6,    6,    6,
         6,    6,    1,    3,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,41 +400,48 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[8] =
+static const YY_CHAR yy_meta[13] =
     {   0,
-        1,    2,    1,    1,    1,    2,    2
+        1,    2,    1,    1,    3,    2,    2,    2,    2,    2,
+        2,    2
     } ;
 
-static const flex_int16_t yy_base[30] =
+static const flex_int16_t yy_base[39] =
     {   0,
-        0,    0,   32,   42,   28,   24,    6,   22,   12,   18,
-       42,   17,    0,   18,   42,   20,    0,   42,   13,   26,
-       10,    7,   42,   32,   36,   42,   42,   42,    7
+        0,    7,   40,   57,   36,   32,   14,   25,   31,   20,
+       24,   57,   25,    0,   25,   15,   57,   22,   30,   57,
+       20,   34,   15,    9,    9,   57,   40,    2,   44,   57,
+        0,   57,   57,   57,   49,    1,   51,   53
     } ;
 
-static const flex_int16_t yy_def[30] =
+static const flex_int16_t yy_def[39] =
     {   0,
-       28,    1,   28,   28,   29,   28,   28,   29,   28,   28,
-       28,   28,    7,   28,   28,    9,    9,   28,   28,   28,
-       28,   28,   28,   28,   28,   28,   28,    0,   28
+       35,   35,   34,   34,   36,   34,   34,   34,   36,   37,
+       34,   34,   34,    7,   34,   34,   34,   38,   37,   34,
+       34,   34,   34,   34,   34,   34,   34,   34,   34,   34,
+       34,   34,   34,    0,   34,   34,   34,   34
     } ;
 
-static const flex_int16_t yy_nxt[50] =
+static const flex_int16_t yy_nxt[70] =
     {   0,
-        4,    5,    4,    6,    7,    4,    4,   11,    9,   12,
-       13,   25,   14,   15,   24,   22,   16,   17,   17,   18,
-       21,   20,   10,    8,   19,   16,   16,   23,   10,    8,
-       20,   28,   19,   26,   28,   28,   24,   27,   28,   28,
-       25,    3,   28,   28,   28,   28,   28,   28,   28
+       34,    5,   10,    6,    7,   34,   34,    8,    5,   33,
+        6,    7,   31,   29,    8,   12,   28,   13,   14,   27,
+       15,   17,   25,   17,   18,   20,   24,   23,   11,   22,
+       21,   17,    9,   16,   18,   26,   11,    9,   22,   34,
+       21,   30,   34,   34,   27,   32,   34,   34,   29,    4,
+        4,    4,   19,   19,   18,   18,    3,   34,   34,   34,
+       34,   34,   34,   34,   34,   34,   34,   34,   34
     } ;
 
-static const flex_int16_t yy_chk[50] =
+static const flex_int16_t yy_chk[70] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    7,   29,    7,
-        7,   22,    7,    9,   21,   19,    9,    9,    9,   10,
-       14,   12,   10,    8,   10,   16,   16,   20,    6,    5,
-       20,    3,   20,   24,    0,    0,   24,   25,    0,    0,
-       25,   28,   28,   28,   28,   28,   28,   28,   28
+        0,    1,   36,    1,    1,    0,    0,    1,    2,   31,
+        2,    2,   28,   25,    2,    7,   24,    7,    7,   23,
+        7,   10,   21,   18,   10,   11,   16,   15,   11,   13,
+       11,   19,    9,    8,   19,   22,    6,    5,   22,    3,
+       22,   27,    0,    0,   27,   29,    0,    0,   29,   35,
+       35,   35,   37,   37,   38,   38,   34,   34,   34,   34,
+       34,   34,   34,   34,   34,   34,   34,   34,   34
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -452,13 +460,14 @@ int yy_flex_debug = 0;
 char *yytext;
 #line 1 "cat.l"
 #line 2 "cat.l"
-#define real 400
-#define identificador 401
-#define COMMENT 402
-#define TEXT 403
-#define COMMAND 404
-#line 461 "lex.yy.c"
-#line 462 "lex.yy.c"
+#define entero 400
+#define real 401
+#define identificador 402
+#define COMMENT 403
+#define TEXT 404
+#define COMMAND 405
+#line 470 "lex.yy.c"
+#line 471 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -675,9 +684,9 @@ YY_DECL
 		}
 
 	{
-#line 9 "cat.l"
+#line 10 "cat.l"
 
-#line 681 "lex.yy.c"
+#line 690 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -704,13 +713,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 29 )
+				if ( yy_current_state >= 35 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 42 );
+		while ( yy_base[yy_current_state] != 57 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -737,39 +746,44 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
-#line 10 "cat.l"
+#line 11 "cat.l"
 ;
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
+YY_RULE_SETUP
 #line 12 "cat.l"
+{return entero;}
+	YY_BREAK
 case 3:
-/* rule 3 can match eol */
-#line 13 "cat.l"
+#line 14 "cat.l"
 case 4:
 /* rule 4 can match eol */
-#line 14 "cat.l"
+#line 15 "cat.l"
 case 5:
 /* rule 5 can match eol */
-#line 15 "cat.l"
+#line 16 "cat.l"
 case 6:
 /* rule 6 can match eol */
-YY_RULE_SETUP
-#line 15 "cat.l"
-{return real;}
-	YY_BREAK
+#line 17 "cat.l"
 case 7:
 /* rule 7 can match eol */
 YY_RULE_SETUP
-#line 16 "cat.l"
-{return identificador;}
+#line 17 "cat.l"
+{return real;}
 	YY_BREAK
 case 8:
+/* rule 8 can match eol */
 YY_RULE_SETUP
-#line 19 "cat.l"
+#line 18 "cat.l"
+{return identificador;}
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 21 "cat.l"
 ECHO;
 	YY_BREAK
-#line 773 "lex.yy.c"
+#line 787 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1066,7 +1080,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 29 )
+			if ( yy_current_state >= 35 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1094,11 +1108,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 29 )
+		if ( yy_current_state >= 35 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 28);
+	yy_is_jam = (yy_current_state == 34);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1774,7 +1788,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 19 "cat.l"
+#line 21 "cat.l"
 
 
 #include <stdio.h>
